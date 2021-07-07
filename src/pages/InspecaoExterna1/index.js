0caba796ba223c08dd0dc1e12dac2526b9719e05
amandals <lima.direito1@gmail.com>
2021-07-07 14:36:24 -0300

    insercao dos pickers

--- src/pages/InspecaoExterna1/index.js
+++ src/pages/InspecaoExterna1/index.js
@@ -1,7 +1,8 @@
 //Importando bibliotecas
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; 
+import { Picker } from '@react-native-picker/picker';  
 
 export default function InspecaoExterna1({ route }) {
   const navigation = useNavigation();
@@ -10,10 +11,62 @@ export default function InspecaoExterna1({ route }) {
     navigation.navigate('InspecaoExterna2');
   }
 
+  const [selectedValue, setSelectedValue] = useState("Selecione Uma opção");
+
   return (
     <View style={{flex: 1, backgroundColor: '#64CAD9'}}>
+
     <View style={{height: 120 , backgroundColor: '#64CAD9'}}></View>
-    <View style={{flex: 1, backgroundColor: '#214E88'}}></View> 
+
+    <View style={{flex: 1, backgroundColor: '#214E88'}}>
+
+    
+    <Picker //criação dos pickers (caixas para seleção de opção )
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+         //lista de itens : (picker.item)
+      > 
+        <Picker.Item label="Java" value="java" /> 
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+    </View> 
     
     <Button //botao para voltar
      title=" Voltar "
@@ -24,6 +77,7 @@ export default function InspecaoExterna1({ route }) {
      title=" Ir para Inspeção Externa 2 "
      onPress={irInspecaoExterna2}
      />
+
     <View style={{height: 120, backgroundColor: '#64CAD9'}}></View>
     </View>
    ); 
--- src/pages/InspecaoExterna5/index.js
+++ src/pages/InspecaoExterna5/index.js
@@ -1,7 +1,8 @@
 //Importando bibliotecas
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function InspecaoExterna5({ route }) {
   const navigation = useNavigation();
@@ -10,11 +11,60 @@ export default function InspecaoExterna5({ route }) {
     navigation.navigate('TelaConfirmacao');
   }
 
+  const [selectedValue, setSelectedValue] = useState("Selecione Uma opção");
+
   return (
     <View style={{flex: 1, backgroundColor: '#64CAD9'}}>
     <View style={{height: 120 , backgroundColor: '#64CAD9'}}></View>
-    <View style={{flex: 1, backgroundColor: '#214E88'}}></View> 
-  
+    <View style={{flex: 1, backgroundColor: '#214E88'}}>
+      
+    <Picker //criação dos pickers (caixas para seleção de opção )
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+         //lista de itens : (picker.item)
+      > 
+        <Picker.Item label="Java" value="java" /> 
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 200, width: 300 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>  
+    </View> 
+
     <Button // botao voltar
      title=" Voltar "
      onPress={ () => navigation.goBack() }
