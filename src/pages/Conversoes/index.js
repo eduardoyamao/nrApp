d5407923ff102886c5c3ad10ce47300d79a9b9ea
amandals <lima.direito1@gmail.com>
2021-07-08 09:41:42 -0300

    adicionando a file de conversoes para a fran preencher

--- src/pages/Conversoes/index.js
+++ src/pages/Conversoes/index.js
@@ -2,35 +2,33 @@ import React, {Component} from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-export default function TelaConfirmacao({ route }) {
+export default function Conversoes({ route }) {
   const navigation = useNavigation();
    //criacao da funcao ir para pagina de inicio
   function irInicio(){
     navigation.navigate('Inicio');
   }
   
-  function irConversoes(){
-    navigation.navigate('Conversoes');
-  }
-
   return (
     <View style={{flex: 1, backgroundColor: '#64CAD9'}}>
     <View style={{height: 120 , backgroundColor: '#64CAD9'}}></View>
-    <View style={{flex: 1, backgroundColor: '#214E88'}}></View> 
+
+    <View style={{flex: 1, backgroundColor: '#214E88'}}>
+
+        <Text /* fran, coloca as coisas aqui dentro que vc for fazer da conversao */ >
+            coloque  tudo aqui 
+        </Text> 
+
+    </View> 
 
     <Button // botao voltar
      title=" Voltar "
      onPress={ () => navigation.goBack() }
      />
 
-    <Button //botao que chama a funcao ir para tela de conversoesauto
+    <Button //botao que chama a funcao ir para inicio
     title="Voltar para Inicio!"
     onPress={ irInicio }
-    />
-
-    <Button //botao que chama a funcao ir para tela de conversoesauto
-    title="Ir para conversões automáticas "
-    onPress={ irConversoes }
     />
     <View style={{height: 120, backgroundColor: '#64CAD9'}}></View>
     </View>
